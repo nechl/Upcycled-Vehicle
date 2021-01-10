f0f34b541e53bbe5a558599755851eed0999d390
nechl <45829474+nechl@users.noreply.github.com>
2021-01-10 17:45:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,54 +5,57 @@ Programm by Daniel Huber and Nicolas Perruchoud
 
 * : high
 _ : low
-
-	 dl dr pl pr
-Pin numb:22 23 24 25
-strgt -> _  _  *  *
-Lturn -> _  *  -  *
-rturn -> *  _  *  -
+not in a good order due to soldering issues
+         A1 A2 B1 B2
+front -> *  _  *  _
+left  -> _  *  *  _
+right -> *  _  _  *
 off   -> _  _  _  _
 """
 
 def Main() -> None:
-        dl = dr = pl = pr = toggle_val = 0 # initiating everything to 0
+        A1 = A2 = B1 = B2 = toggle_val = 0 # initiating everything to 0
         while True:
                 v = mouse.read(6) # non-blocking input check
                 if not v:
                         print(".",end="")
                         continue      # no input to handle
 
-                if v == '\n\x00\x00\x08\x00\x00':#RIGHT mouse button is pressed, direction l <-, r ->
-                        dl = 0         # clear direction
-                        dr = 1
-                        pl = 0 # turn general speed either on or off
-                        pr = 1
+                if v == '\n\x00\x00\x08\x00\x00':#RIGHT mouse button is pressed, direction l <-, r ->, now turning right
+                        A1 = 1         
+                        A2 = 0
+                        B1 = 0 
+                        B2 = 1
 
                 elif v == '\t\x00\x00\x08\x00\x00':		# left mouse button is pressed	
                         # direction l ->, r <-
-                        dl = 1         # clear direction
-                        dr = 0
-                        pl = 1 # turn general speed either on or off
-                        pr = 0
+                        A1 = 0        
+                        A2 = 1
+                        B1 = 1 
+                        B2 = 0
 
                 elif v == '\x08\x00\x00\x08\x00\x00':	# middle mouse button is rolled
                         mouse.flush() #
                         # turn wheels either on or off
                         toggle_val = not toggle_val         # toggle between 1 and 0
-                        dl = 0         # clear direction
-                        dr = 0
-                        pl = toggle_val # turn general speed either on or off
-                        pr = toggle_val
+                        A1 = toggle_val        
+                        A2 = 0
+                        B1 = toggle_val 
+                        B2 = 0
 
                 else:
                         print("Invalid io read: ", v)
                         mouse.flush()
 
 	#write the new values to the pins
-                GPIO.output(22, dl)
-                GPIO.output(23, dr)
-                GPIO.output(24, pl)
-                GPIO.output(25, pr)
+                #motor A
+                GPIO.output(24, A1)
+                GPIO.output(23, A2)
+                #motor B
+                GPIO.output(22, B1)     
+                GPIO.output(25, B2)
+
+                
 
 
 
