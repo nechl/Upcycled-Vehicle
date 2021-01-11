d7f1dacc996056341982bec681a1a772312472fd
nechl <45829474+nechl@users.noreply.github.com>
2021-01-11 18:13:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,25 +15,42 @@ off   -> _  _  _  _
 
 def Main() -> None:
         A1 = A2 = B1 = B2 = toggle_val = 0 # initiating everything to 0
+        turningLeft = turningRight= False
         while True:
                 v = mouse.read(6) # non-blocking input check
                 if not v:
                         print(".",end="")
                         continue      # no input to handle
 
-                if v == '\n\x00\x00\x08\x00\x00':#RIGHT mouse button is pressed, direction l <-, r ->, now turning right
+                if v == '\n\x00\x00\x08\x00\x00'and turningRight == False: #RIGHT mouse button is pressed, direction l <-, r ->, now turning right, only if not turning right now
                         A1 = 1         
                         A2 = 0
                         B1 = 0 
                         B2 = 1
+                        turningRight = True
 
-                elif v == '\t\x00\x00\x08\x00\x00': #left mouse button is pressed	
+                elif v == '\t\x00\x00\x08\x00\x00' and turningLeft == False: #left mouse button is pressed	, only if not turning right now
                         # direction l ->, r <-
                         A1 = 0        
                         A2 = 1
                         B1 = 1 
                         B2 = 0
+                        turningLeft = True
+                
+                elif v == '\n\x00\x00\x08\x00\x00'and turningRight == True:
+                        A1 = 0
+                        A2 = 0
+                        B1 = 0
+                        B2 = 0
+                        turningRight = False
 
+                elif v == '\t\x00\x00\x08\x00\x00' and turningLeft == True:
+                        A1 = 0
+                        A2 = 0
+                        B1 = 0
+                        B2 = 0
+                        turningLeft = False   
+                                           
                 elif v == '\x08\x00\x00\x08\x00\x00':	# middle mouse button is rolled
                         mouse.flush() #
                         # turn wheels either on or off
